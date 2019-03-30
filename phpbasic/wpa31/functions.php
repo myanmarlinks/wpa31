9fd2416846188521fd2f1c4779eac91d7fb7c4f6
phpcrazy <phpcrazy@gmail.com>
2019-03-30 17:49:36 +0630

    Language File Switcher

--- phpbasic/wpa31/functions.php
+++ phpbasic/wpa31/functions.php
@@ -13,7 +13,20 @@ function make_view($view) {
 }
 
 function _lang($lang) {
+    $e_lang = explode(".", $lang);
     $default_lang = _config("app.lang");
+    $file = DD . "/app/lang/" . $default_lang . "/" . $e_lang[0] . ".php";
+    if(file_exists($file)) {
+        $lang_value = require $file;
+        if(array_key_exists($e_lang[1], $lang_value)) {
+            return $lang_value[$e_lang[1]];
+        } else {
+            trigger_error("Language key not found", E_USER_ERROR);
+        }
+    } else {
+        trigger_error("Language file not found", E_USER_ERROR);
+    }
+
     
 }
 
