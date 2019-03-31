bc18ae596951a7dd350c36359ff1200825b9f82e
phpcrazy <phpcrazy@gmail.com>
2019-03-31 17:59:03 +0630

    Blog Style

--- phpbasic/app/controller/controllers.php
+++ phpbasic/app/controller/controllers.php
@@ -13,7 +13,26 @@ function _BlogController($category = null, $id = null) {
 } 
 
 function _HomeController() {
-    make_view("home");
+    $data = [
+        "blogs" => [
+            [
+                'id'    => 1,
+                 'title' => 'Why do we use it?',
+                'body'  => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+            ],
+            [
+                'id'    => 2,
+                 'title' => 'Why do we use it?',
+                'body'  => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+            ],
+            [
+                'id'    => 3,
+                 'title' => 'Why do we use it?',
+                'body'  => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+            ]
+        ]
+    ];
+    make_view("home", $data);
 }
 
 function _PageController() {
--- phpbasic/app/view/home.php
+++ phpbasic/app/view/home.php
@@ -1,5 +1,23 @@
-<?php
-
-echo "Home Page";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <?php foreach($blogs as $blog) { ?> 
+                <div class="col-12">
+                <h3><?php echo $blog["id"]; ?></h3>
+                <h1><?php echo $blog["title"]; ?></h1>
+                <p><?php echo $blog['body']; ?></p>
+                </div>        
+            <?php } ?>
+        </div>
+    </div>
+</body>
+</html>
